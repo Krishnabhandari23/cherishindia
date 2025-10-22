1b2168a3004c6376ece986243d29f8a3f8e06ebb
Krishna bhandari <krisbhandari23@gmail.com>
2025-10-22 10:47:47 +0530

    fixed backend issues concurrency

--- server/server.js
+++ server/server.js
@@ -220,9 +220,7 @@ mongoose.connect(MONGO, {
   serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
   maxPoolSize: 10, // Maintain up to 10 socket connections
-  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-  bufferMaxEntries: 0, // Disable mongoose buffering
-  bufferCommands: false, // Disable mongoose buffering
+  // Note: older/deprecated mongoose options removed for compatibility
 })
   .then(()=>{
     console.log('✅ MongoDB connected successfully');
